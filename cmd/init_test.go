adaa5fca6982d47f9df6e1b60a9e5262dbbf2bc4
Antonio Pagano <apagano@wawand.co>
2015-12-01 20:30:41 -0500

    [testing] adding init tests

--- cmd/init_test.go
+++ cmd/init_test.go
@@ -1,48 +1,53 @@
 package cmd
 
-// func init() {
-// 	utils.ClearTestTables()
-// 	setupTestingEnv()
-// }
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
 
-//
-// func TestInitCommand(t *testing.T) {
-// 	setupTestingEnv()
-// 	utils.ClearTestMigrations()
-//
-// 	context := cli.Context{}
-// 	Init(&context)
-// 	base := os.Getenv("TRANS_TESTING_FOLDER")
-//
-// 	files := []string{
-// 		filepath.Join(base, "db"),
-// 		filepath.Join(base, "db", "migrations"),
-// 		filepath.Join(base, "db", "config.yml"),
-// 	}
-//
-// 	for _, file := range files {
-// 		isThere, _ := exists(file)
-// 		assert.True(t, isThere)
-// 	}
-//
-// 	// //Content
-// 	content, _ := ioutil.ReadFile(files[2])
-// 	assert.Contains(t, string(content), "development")
-// 	assert.Contains(t, string(content), "url")
-// 	assert.Contains(t, string(content), "driver")
-//
-// }
-//
-// func TestInitExistingFolder(t *testing.T) {
-// 	setupTestingEnv()
-// 	base := os.Getenv("TRANS_TESTING_FOLDER")
-//
-// 	os.RemoveAll(base)
-// 	os.Mkdir(filepath.Join(base), generatedFilePermissions)
-// 	os.Mkdir(filepath.Join(base, "db"), generatedFilePermissions)
-//
-// 	context := cli.Context{}
-// 	Init(&context)
-// 	isThere, _ := exists(filepath.Join(base, "db", "migrations"))
-// 	assert.False(t, isThere)
-// }
+	"github.com/wawandco/transporter/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/wawandco/transporter/Godeps/_workspace/src/github.com/stretchr/testify/assert"
+	"github.com/wawandco/transporter/utils"
+)
+
+func TestInitCommand(t *testing.T) {
+	setupTestingEnv()
+	utils.ClearTestMigrations()
+
+	context := cli.Context{}
+	Init(&context)
+	base := os.Getenv("TRANS_TESTING_FOLDER")
+
+	files := []string{
+		filepath.Join(base, "db"),
+		filepath.Join(base, "db", "migrations"),
+		filepath.Join(base, "db", "config.yml"),
+	}
+
+	for _, file := range files {
+		isThere, _ := exists(file)
+		assert.True(t, isThere)
+	}
+
+	// //Content
+	content, _ := ioutil.ReadFile(files[2])
+	assert.Contains(t, string(content), "development")
+	assert.Contains(t, string(content), "url")
+	assert.Contains(t, string(content), "driver")
+
+}
+
+func TestInitExistingFolder(t *testing.T) {
+	setupTestingEnv()
+	base := os.Getenv("TRANS_TESTING_FOLDER")
+
+	os.RemoveAll(base)
+	os.Mkdir(filepath.Join(base), generatedFilePermissions)
+	os.Mkdir(filepath.Join(base, "db"), generatedFilePermissions)
+
+	context := cli.Context{}
+	Init(&context)
+	isThere, _ := exists(filepath.Join(base, "db", "migrations"))
+	assert.False(t, isThere)
+}
